150e186a53b2a69d3400ad55ef9d59521851f02f
RaquelAFerreira <raquelalmferreira@gmail.com>
2025-06-18 18:43:39 -0300

    terceiro

---
+++ condicional/temperatura.py
@@ -0,0 +1,6 @@
+temperatura = input("Digite a temperatura atual: ")
+
+if int(temperatura) > 25:
+    input("Alerta! Temperatura acima do limite permitido.")
+else:
+    input("Temperatura normal.")
