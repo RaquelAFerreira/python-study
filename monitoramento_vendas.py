5aa9ea8e542679d5a16950fff841ec77e728da14
RaquelAFerreira <raquelalmferreira@gmail.com>
2025-06-18 18:18:57 -0300

    Atividade: Monitoramento de vendas no console

---
+++ monitoramento_vendas.py
@@ -0,0 +1,7 @@
+macas = int(input("Digite a quantidade de maçãs vendidas: "))
+bananas = int(input("Digite a quantidade de maçãs vendidas: "))
+
+if macas > bananas:
+    input("As maçãs tiveram mais vendidas")
+else:
+    input("As bananas tiveram mais vendidas")
